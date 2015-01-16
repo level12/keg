3ba355a782d06c5ff3748420a9e49e8083443ad0
Randy Syring <randy.syring@level12.io>
2015-01-15 22:13:59 -0500

    add @classproperty and adjust cli methods on Keg

--- keg/utils.py
+++ keg/utils.py
@@ -34,3 +34,15 @@ def ensure_dirs(newdir, mode=NotGiven):
 def reraise_lastexc():
     exc_type, exc_value, tb = sys.exc_info()
     reraise(exc_type, exc_value, tb)
+
+
+class classproperty(property):
+    """A decorator that behaves like @property except that operates
+    on classes rather than instances.
+    """
+    def __init__(self, fget, *arg, **kw):
+        super(classproperty, self).__init__(fget, *arg, **kw)
+        self.__doc__ = fget.__doc__
+
+    def __get__(desc, self, cls):
+        return desc.fget(cls)
