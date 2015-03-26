4d70d33f15b5776f29406c80d8f982f03514fff7
Randy Syring <randy.syring@level12.io>
2015-03-26 01:34:28 -0400

    continuing to work on web views

--- keg/web.py
+++ keg/web.py
@@ -2,13 +2,17 @@ from __future__ import absolute_import
 from __future__ import unicode_literals
 
 import inspect
+import sys
 
 from blazeutils.strings import case_cw2us, case_cw2dash, simplify_string
 import flask
 from flask import request
 from flask.views import MethodView, MethodViewType, http_method_funcs
+import six
 from werkzeug.datastructures import MultiDict
 from werkzeug.utils import validate_arguments, ArgumentValidationError
+
+from keg.compat import with_metaclass
 
 
 class ImmediateResponse(Exception):
@@ -45,7 +49,7 @@ def _werkzeug_multi_dict_conv(md):
         if only one value or a list if multiple values
     '''
     retval = {}
-    for key, value in md.to_dict(flat=False).iteritems():
+    for key, value in six.iteritems(md.to_dict(flat=False)):
         if len(value) == 1:
             retval[key] = value[0]
         else:
@@ -55,7 +59,7 @@ def _werkzeug_multi_dict_conv(md):
 
 def _call_with_expected_args(view, calling_args, method, method_is_bound=True):
     """ handle argument conversion to what the method accepts """
-    if isinstance(method, basestring):
+    if isinstance(method, six.string_types):
         if not hasattr(view, method):
             return
         method = getattr(view, method)
@@ -66,7 +70,7 @@ def _call_with_expected_args(view, calling_args, method, method_is_bound=True):
         # removing it before the bound method is called below
         pos_args = (view,) if method_is_bound else tuple()
         args, kwargs = validate_arguments(method, pos_args, calling_args.copy())
-    except ArgumentValidationError, e:
+    except ArgumentValidationError as e:
         msg = 'Argument mismatch occured: method=%s, missing=%s, extra_keys=%s, extra_pos=%s.' \
               '  Arguments available: %s' % (method, e.missing, e.extra, e.extra_positional,
                                              calling_args)
@@ -83,20 +87,17 @@ def _call_with_expected_args(view, calling_args, method, method_is_bound=True):
 class _ViewMeta(MethodViewType):
 
     def __new__(cls, clsname, bases, attr):
+        rules = attr.pop('_rules', [])
         viewcls = super(_ViewMeta, cls).__new__(cls, clsname, bases, attr)
         viewcls.init_routes()
         if viewcls.blueprint is not None:
-            viewcls.init_blueprint()
+            viewcls.init_blueprint(rules)
         return viewcls
 
 
-class BaseView(MethodView):
-    __metaclass__ = _ViewMeta
-
-    automatic_route = True
+class BaseView(with_metaclass(_ViewMeta, MethodView)):
     blueprint = None
     url = None
-    url_rules = None
     template_name = None
     _index_method = None
 
@@ -115,8 +116,6 @@ class BaseView(MethodView):
             method_name = 'xhr'
         else:
             method_name = request.method.lower()
-            if method_name == 'get' and self._index_method is not None:
-                    method_name = self._index_method
 
         method_obj = getattr(self, method_name, None)
 
@@ -154,7 +153,7 @@ class BaseView(MethodView):
         # start with query string arguments that are expected
         args = MultiDict()
         if self.expected_qs_args:
-            for k in request.args.iterkeys():
+            for k in six.iterkeys(request.args):
                 if k in self.expected_qs_args:
                     args.setlist(k, request.args.getlist(k))
 
@@ -162,7 +161,7 @@ class BaseView(MethodView):
         # arguments get precedence and we don't want to just .update()
         # because that would allow arbitrary get arguments to affect the
         # values of the URL arguments
-        for k, v in urlargs.iteritems():
+        for k, v in six.iteritems(urlargs):
             args[k] = v
 
         return _werkzeug_multi_dict_conv(args)
@@ -213,12 +212,18 @@ class BaseView(MethodView):
         return case_cw2dash(cls.__name__)
 
     @classmethod
-    def add_url_rule(cls, method_name, options, use_class_url):
+    def add_url_rule(cls, method_name, options, use_class_url, method_endpoint=None):
         class_url = cls.calc_url()
         dashed_method_name = case_cw2dash(method_name)
         method_url = '{}/{}'.format(class_url, dashed_method_name)
 
         default_rule = class_url if use_class_url else method_url
+        if use_class_url:
+            default_rule = class_url
+            options.setdefault('endpoint', cls.calc_endpoint())
+        else:
+            default_rule = method_url
+            options.setdefault('endpoint', method_endpoint)
 
         rule = options.pop('rule')
         if not rule:
@@ -227,17 +232,18 @@ class BaseView(MethodView):
             # since there is no slash, it is a relative URL
             rule = '{}/{}'.format(default_rule, rule)
 
+        # Methods was a set for easier adds w/out duplicates, but convert back to list now.
         cls.url_rules.append((rule, options))
 
     @classmethod
     def init_routes(cls):
-        if cls.url_rules is None:
-            cls.url_rules = []
+        cls.url_rules = []
 
         def method_with_rules(obj):
-            return inspect.ismethod(obj) and hasattr(obj, '_rule_options')
+            # isroutine() matches functions (PY3) and unbound methods (PY2).
+            return inspect.isroutine(obj) and hasattr(obj, '_rule_options')
 
-        index_method = None
+        default_method = None
         for method_name, method_obj in inspect.getmembers(cls, predicate=method_with_rules):
             # Since we are popping from and adding to the options, make a copy of them.  This
             # prevents problems when subclassing.
@@ -246,10 +252,10 @@ class BaseView(MethodView):
             dashed_method_name = case_cw2dash(method_name)
             class_endpoint = cls.calc_endpoint()
             method_endpoint = '{}:{}'.format(class_endpoint, dashed_method_name)
-            method_endpoint = options.setdefault('endpoint', method_endpoint)
 
-            view_func_name = simplify_string(method_endpoint.replace(':', '_'), replace_with='_')
-            options['view_func'] = cls.as_view(view_func_name.encode('ascii'), method_name)
+            view_func_name = simplify_string('{}_{}'.format(class_endpoint, method_name),
+                                             replace_with='_')
+            options['view_func'] = cls.as_view(str(view_func_name), method_name)
 
             # If this method is named after an http method, then we we have special handling.
             if method_name in http_method_funcs:
@@ -257,30 +263,42 @@ class BaseView(MethodView):
                 # DELETE /blog/1234
                 verb_options = options.copy()
                 verb_options['methods'].add(method_name.upper())
-                cls.add_url_rule(method_name, options.copy(), use_class_url=True)
+                cls.add_url_rule(method_name, options.copy(), True)
 
                 # Now, create a GET related rule with a URL segment that matches the method.  This
                 # responds to a request like: GET /blog/delete/1234
                 if 'GET' in options['methods'] and method_name != 'get':
-                    cls.add_url_rule(method_name, options, use_class_url=False)
-            elif options.pop('index', None):
-                    assert index_method is None, 'More than one route method has been specified' \
-                        ' as the index.'
-                    index_method = method_name
-                    cls.add_url_rule(method_name, options, use_class_url=True)
+                    cls.add_url_rule(method_name, options, False, method_endpoint)
+            elif options.pop('to_class', False):
+                    assert default_method is None, 'More than one route method has been specified' \
+                        ' to be routed at the class level using `to_class`.'
+                    default_method = method_name
+                    cls.add_url_rule(method_name, options, True)
             else:
-                cls.add_url_rule(method_name, options, use_class_url=False)
-        cls._index_method = index_method
+                cls.add_url_rule(method_name, options, False, method_endpoint)
 
     @classmethod
-    def init_blueprint(cls):
+    def init_blueprint(cls, rules):
         endpoint = cls.calc_endpoint()
-        # Flask assigns the endpoint to the __name__ atribute of the function it creates, so we need
-        # to make sure it's a byte string or Python will throw an exception.
-        bytes_endpoint = endpoint.encode('utf-8')
-        view_func = cls.as_view(bytes_endpoint)
+        class_url = cls.calc_url()
+
         if cls.methods:
-            cls.blueprint.add_url_rule(cls.calc_url(), endpoint=endpoint, view_func=view_func)
+            # Flask assigns the endpoint to the __name__ atribute of the function it creates and
+            # therefore the endpoint must be a `str`.
+            str_endpoint = str(endpoint)
+            view_func = cls.as_view(str_endpoint)
+
+            absolute_found = False
+            for rule, options in rules:
+                if rule.startswith('/'):
+                    absolute_found = True
+                    class_url = rule
+                else:
+                    rule = '{}/{}'.format(class_url, rule)
+                cls.blueprint.add_url_rule(rule, endpoint=endpoint, view_func=view_func, **options)
+
+            if not absolute_found:
+                cls.blueprint.add_url_rule(class_url, endpoint=endpoint, view_func=view_func)
         for rule, options in cls.url_rules:
             cls.blueprint.add_url_rule(rule, **options)
 
@@ -303,3 +321,14 @@ def route(rule=None, get=True, post=False, methods=None, **options):
         return func
 
     return wrapper
+
+
+class ViewRule(object):
+    def __init__(self, rule, kwargs):
+        self.arg_pair = (rule, kwargs)
+
+
+def rule(rule=None, **kwargs):
+    parent_locals = sys._getframe(1).f_locals
+    rules = parent_locals.setdefault('_rules', [])
+    rules.append((rule, kwargs))
