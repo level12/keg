a4b15d6feedc5136cbc3d9a92f9c869bb7957be7
Randy Syring <randy@thesyrings.us>
2015-03-27 01:44:45 -0400

    fix build-wheelhouse.py and add packages for travis

--- requirements/build-wheelhouse.py
+++ requirements/build-wheelhouse.py
@@ -2,11 +2,12 @@ import subprocess
 import pathlib
 
 cdir = pathlib.Path(__file__).parent
+wheeldir_dpath = str(cdir.joinpath('wheelhouse'))
 
 
 def build(req_fpath):
-    pip_args = ['wheel', '--wheel-dir=wheelhouse', '--use-wheel', '--find-links=wheelhouse',
-                '-r']
+    pip_args = ['wheel', '--wheel-dir', wheeldir_dpath, '--use-wheel', '--find-links',
+                wheeldir_dpath, '-r']
     subprocess.check_call(['pip'] + pip_args + [req_fpath])
     subprocess.check_call(['pip3.4'] + pip_args + [req_fpath])
 
