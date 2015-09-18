a676749024344d806cb759077d7616ee5cb965b1
Nick Zaccardi <nicholas.zaccardi@gmail.com>
2015-09-18 17:03:22 -0400

    Remove all instances of unicode_literals

--- keg/oauth/__init__.py
+++ keg/oauth/__init__.py
@@ -1,5 +1,4 @@
 from __future__ import absolute_import
-from __future__ import unicode_literals
 
 from flask import url_for, session, request, jsonify, Blueprint, flash, current_app
 from flask_oauthlib.client import OAuth
