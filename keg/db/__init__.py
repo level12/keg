985054d61af5ea9dacffc7fbf397c836190abe1e
Nick Zaccardi <nicholas.zaccardi@gmail.com>
2016-12-16 16:32:32 -0600

    Remove deprecated hooks
    Fixes GH-46

--- keg/db/__init__.py
+++ keg/db/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import
 
-from flask.ext.sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 
 from keg.signals import testing_run_start, db_init_pre, db_init_post, db_clear_pre, db_clear_post
 from keg.utils import visit_modules
@@ -16,7 +16,7 @@ class KegSQLAlchemy(SQLAlchemy):
     def testing_scoped_session(self):
         # don't want to have to import this if we are in production, so put import
         # inside of the method
-        from flask.ext.webtest import get_scopefunc
+        from flask_webtest import get_scopefunc
 
         # flask-sqlalchemy creates the session when the class is initialized.  We have to re-create
         # with different session options and override the session attribute with the new session
