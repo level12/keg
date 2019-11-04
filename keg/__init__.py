2d6bf3a83dc94f34dc8555685fdf86cef3510025
Matt Lewellyn <guruofgentoo@gmail.com>
2019-11-04 08:20:47 -0500

    add __all__ to package

--- keg/__init__.py
+++ keg/__init__.py
@@ -4,3 +4,5 @@ from flask import current_app  # noqa: F401
 
 from keg.app import Keg  # noqa: F401
 from keg.component import KegComponent  # noqa: F401
+
+__all__ = ['Keg', 'KegComponent', 'current_app']
